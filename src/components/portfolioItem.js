be676efa732b32e50265d148cb8af2058889d00c
chrillah <ch.ronnberg@gmail.com>
2023-12-18 15:44:16 +0100

    update

--- src/components/portfolioItem.js
+++ src/components/portfolioItem.js
@@ -2,17 +2,21 @@ import * as React from "react"
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
 
 const PortfolioItem = ({ item, lengthOfItems, indexOfItem }) => {
+  const image = getImage(item.portfolioHeroImage)
   return (
     <li className="portfolio-item">
       <div className="item-top-container">
         <h3>{item.timePeriod}</h3>
         <h3>{item.portfolioSubject}</h3>
       </div>
-      <div className="item-middle-container">
+      <div className="item-image-container">
         <GatsbyImage
-          image={getImage(item.portfolioHeroImage)}
+          className="item-image"
+          image={image}
           alt={item.portfolioTitle}
         />
+      </div>
+      <div className="item-middle-container">
         <h1>{item.portfolioTitle}</h1>
         <p>{item.portfolioDescription.portfolioDescription}</p>
       </div>
