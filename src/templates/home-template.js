55ae89f5b6f5698bffedbe426036962a31de0cec
chrillah <ch.ronnberg@gmail.com>
2024-01-05 17:19:16 +0100

    update

--- src/templates/home-template.js
+++ src/templates/home-template.js
@@ -1,5 +1,8 @@
 import * as React from "react"
 import Layout from "../components/layout"
+import { BLOCKS } from "@contentful/rich-text-types"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
+import { renderRichText } from "gatsby-source-contentful/rich-text"
 
 // Add later?
 // import Seo from "../components/seo"
@@ -8,12 +11,24 @@ import Layout from "../components/layout"
 // import { GatsbyImage, getImage } from "gatsby-plugin-image"
 
 const HomeTemplate = contentfulPage => {
+  const richTextConfig = {
+    renderNode: {
+      [BLOCKS.HEADING_1]: (node, children) => <h1>{children}</h1>,
+    },
+  }
+
+  const image = getImage(contentfulPage.image)
+
   return (
     <Layout>
-      <div className="app-page-container">
-        <div className="app-container">
-          <div className="hero-wrapper">
-            <h1>{contentfulPage.title}</h1>
+      <div className="gradient-container">
+        <div className="app-page-container">
+          <div className="app-container">
+            <div>{renderRichText(contentfulPage.content, richTextConfig)}</div>
+            <GatsbyImage
+              image={image}
+              alt="image"
+            />
           </div>
         </div>
       </div>
