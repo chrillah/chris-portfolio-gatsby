837fe23f9ca422185f01368accf7145ed504fdd6
chrillah <ch.ronnberg@gmail.com>
2024-01-10 11:44:50 +0100

    update

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,20 +1,20 @@
 import * as React from "react"
-import { useStaticQuery, graphql } from "gatsby"
+// import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
 import Footer from "./footer"
 import "../css/style.css"
 
 const Layout = ({ children }) => {
-    const data = useStaticQuery(graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `)
+    // const data = useStaticQuery(graphql`
+    //   query SiteTitleQuery {
+    //     site {
+    //       siteMetadata {
+    //         title
+    //       }
+    //     }
+    //   }
+    // `)
 
   return (
     <>
