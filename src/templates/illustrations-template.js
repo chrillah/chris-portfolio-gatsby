b417f1f437132b59047f96e62dcdd8d2f02df9cd
chrillah <ch.ronnberg@gmail.com>
2024-01-10 11:14:42 +0100

    update

--- src/templates/illustrations-template.js
+++ src/templates/illustrations-template.js
@@ -1,5 +1,4 @@
 import * as React from "react"
-import { Link } from "gatsby"
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import Layout from "../components/layout"
 
@@ -8,22 +7,6 @@ const IllustrationsTemplate = contentfulPage => {
     <Layout>
       <div className="app-page-container">
         <div className="app-container">
-        <Link className="back-button" to="/">
-            <svg
-              className="arrow rev"
-              id="Layer_1"
-              data-name="Layer 1"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 30 30"
-            >
-              <polyline
-                className="arrow-detail"
-                points="14.95 29.44 29.39 15 14.95 .56"
-              />
-              <line className="arrow-detail" x1="29.39" y1="15" y2="15" />
-            </svg>
-            <p>Back</p>
-          </Link>
           <h1 className="app-header">{contentfulPage.title}</h1>
           <div className="gallery-container">
             {contentfulPage.gallery.map((image, index) => {
--- src/templates/portfolio-template.js
+++ src/templates/portfolio-template.js
@@ -12,24 +12,8 @@ const PortfolioTemplate = () => {
       <div className="gradient-container">
         <div className="app-page-container">
           <div className="app-container">
-            <Link className="back-button" to="/">
-              <svg
-                className="arrow rev"
-                id="Layer_1"
-                data-name="Layer 1"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 30 30"
-              >
-                <polyline
-                  className="arrow-detail"
-                  points="14.95 29.44 29.39 15 14.95 .56"
-                />
-                <line className="arrow-detail" x1="29.39" y1="15" y2="15" />
-              </svg>
-              <p>Back</p>
-            </Link>
             <div className="portfolio-hero-container">
-              <h1 className="app-header">Index</h1>
+              <h1 className="hero-title">Index</h1>
               <ul className="project-list">
                 {portfolioInformation.map((edge, index) => {
                   return (
