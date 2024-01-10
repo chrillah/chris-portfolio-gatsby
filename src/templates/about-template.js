9645e37836654df917264a4886dae5ba294ba940
chrillah <ch.ronnberg@gmail.com>
2024-01-10 16:35:25 +0100

    update

--- src/templates/about-template.js
+++ src/templates/about-template.js
@@ -5,6 +5,7 @@ import { renderRichText } from "gatsby-source-contentful/rich-text"
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import useEducationInformation from "../hooks/use-educationInformation"
 import EducationItem from "../components/educationItem"
+import { Link } from "gatsby"
 
 const AboutTemplate = contentfulPage => {
   const educationInformation = useEducationInformation()
@@ -17,24 +18,20 @@ const AboutTemplate = contentfulPage => {
 
   return (
     <Layout>
-      <div className="app-page-container">
-        <div className="app-container">
-          <div className="about-hero-container">
-            <div>
-              <h1 className="hero-title">{contentfulPage.title}</h1>
-              <div className="about-description">
-                {renderRichText(contentfulPage.content, richTextConfig)}
+      <div className="gradient-container">
+        <div className="app-page-container">
+          <div className="app-container">
+            <div className="about-hero-container">
+              <div>
+                <h1 className="hero-title">{contentfulPage.title}</h1>
+                <div className="about-description">
+                  {renderRichText(contentfulPage.content, richTextConfig)}
+                </div>
               </div>
+              <GatsbyImage image={image} alt={contentfulPage.title} />
             </div>
-            <GatsbyImage image={image} alt={contentfulPage.title} />
-          </div>
-        </div>
-      </div>
-      {educationInformation ? (
-        <>
-          <div className="gradient-container">
-            <div className="app-page-container">
-              <div className="app-container">
+            {educationInformation ? (
+              <>
                 <div className="education-container">
                   <div className="education-item">
                     <h1 className="hero-title">Education</h1>
@@ -68,13 +65,28 @@ const AboutTemplate = contentfulPage => {
                     )
                   })}
                 </div>
+              </>
+            ) : (
+              <></>
+            )}
+            <div className="about-page-bottom-container">
+              <h3>Check out the rest.</h3>
+              <div>
+                <div>
+                  <Link to="/projects" className="link-button">
+                    Projects
+                  </Link>
+                </div>
+                <div>
+                  <Link to="/illustrations" className="link-button">
+                    Illustrations
+                  </Link>
+                </div>
               </div>
             </div>
           </div>
-        </>
-      ) : (
-        <></>
-      )}
+        </div>
+      </div>
     </Layout>
   )
 }
