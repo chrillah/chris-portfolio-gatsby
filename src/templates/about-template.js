8922f34d1171eeae0d712a199f636681a1af6e57
Christopher Rönnberg <ch.ronnberg@gmail.com>
2024-01-11 11:10:56 +0100

    update

--- src/templates/about-template.js
+++ src/templates/about-template.js
@@ -35,24 +35,6 @@ const AboutTemplate = contentfulPage => {
                 <div className="education-container">
                   <div className="education-item">
                     <h1 className="hero-title">Education</h1>
-                    <svg
-                      className="arrow"
-                      id="Layer_1"
-                      data-name="Layer 1"
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 30 30"
-                    >
-                      <polyline
-                        className="arrow-detail"
-                        points="14.95 29.44 29.39 15 14.95 .56"
-                      />
-                      <line
-                        className="arrow-detail"
-                        x1="29.39"
-                        y1="15"
-                        y2="15"
-                      />
-                    </svg>
                   </div>
                   {educationInformation.map((edge, index) => {
                     return (
