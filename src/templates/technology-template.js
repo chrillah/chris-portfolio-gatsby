b14c3ee0f19762e55ab36c03e8659ce43177db53
chrillah <ch.ronnberg@gmail.com>
2024-01-15 14:30:14 +0100

    update

--- src/templates/technology-template.js
+++ src/templates/technology-template.js
@@ -26,89 +26,95 @@ const TechnologyTemplate = contentfulPage => {
   ]
   return (
     <Layout>
-      <div className="app-page-wrapper">
-        <div className="app-container">
-          <div className="technology-hero-container">
-            <div className="technology-hero-item-1">
-              <div className="technology-presentation">
-                <h3>{contentfulPage.title}</h3>
-                <div>{renderRichText(contentfulPage.body, richTextConfig)}</div>
+      <div className="gradient-bg">
+        <div className="app-page-wrapper">
+          <div className="app-container">
+            <div className="technology-hero-container">
+              <div className="technology-hero-item-1">
+                <div className="technology-presentation">
+                  <h3>{contentfulPage.title}</h3>
+                  <div>
+                    {renderRichText(contentfulPage.body, richTextConfig)}
+                  </div>
+                </div>
+                {renderRichText(contentfulPage.content, richTextConfig)}
               </div>
-              {renderRichText(contentfulPage.content, richTextConfig)}
-            </div>
 
-            <div className="technology-hero-item-2">
-              <h3>Select category</h3>
-              <div>
-                <div className="technology-select-container">
-                  <select
-                    className="technology-select"
-                    value={selectedCategory}
-                    onChange={e => setSelectedCategory(e.target.value)}
-                  >
-                    {cateroryList.map((category, index) => {
-                      return (
-                        <option
-                          className="technology-option"
-                          key={index}
-                          value={category}
-                        >
-                          {category}
-                        </option>
-                      )
-                    })}
-                  </select>
-                  <span className="focus"></span>
+              <div className="technology-hero-item-2">
+                <h3>Select category</h3>
+                <div>
+                  <div className="technology-select-container">
+                    <select
+                      className="technology-select"
+                      value={selectedCategory}
+                      onChange={e => setSelectedCategory(e.target.value)}
+                    >
+                      {cateroryList.map((category, index) => {
+                        return (
+                          <option
+                            className="technology-option"
+                            key={index}
+                            value={category}
+                          >
+                            {category}
+                          </option>
+                        )
+                      })}
+                    </select>
+                    <span className="focus"></span>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-      <ul className="technology-container">
-        {technologyInformation.map((technology, index) => {
-          if (
-            technology.node.category === selectedCategory ||
-            selectedCategory === "All"
-          ) {
-            const logo = getImage(technology.node.logo)
-            return (
-              <li
-                key={index}
-                className={
-                  technology.node.title === "PHP"
-                    ? "technology-item color-set-1"
-                    : technology.node.title === "GatsbyJS"
-                    ? "technology-item color-set-2"
-                    : technology.node.title === "Contentful"
-                    ? "technology-item color-set-3"
-                    : "technology-item"
-                }
-              >
-                <div className="technology-item-top">
-                  <p className="technology-category">
-                    {technology.node.category}
-                  </p>
-                  <div className="technology-logo-container">
-                    <GatsbyImage
-                      image={logo}
-                      alt="image"
-                      className="technology-logo"
-                    />
+        <ul className="technology-container">
+          {technologyInformation.map((technology, index) => {
+            if (
+              technology.node.category === selectedCategory ||
+              selectedCategory === "All"
+            ) {
+              const logo = getImage(technology.node.logo)
+              return (
+                <li
+                  key={index}
+                  className={
+                    technology.node.title === "PHP"
+                      ? "technology-item color-set-1"
+                      : technology.node.title === "GatsbyJS"
+                      ? "technology-item color-set-2"
+                      : technology.node.title === "Contentful"
+                      ? "technology-item color-set-3"
+                      : "technology-item"
+                  }
+                >
+                  <div className="technology-item-top">
+                    <p className="technology-category">
+                      {technology.node.category}
+                    </p>
+                    <div className="technology-logo-container">
+                      <GatsbyImage
+                        image={logo}
+                        alt="image"
+                        className="technology-logo"
+                      />
+                    </div>
                   </div>
-                </div>
-                <div className="technology-item-bottom">
-                  <h3 className="technology-header">{technology.node.title}</h3>
-                  <p className="technology-description">
-                    {technology.node.description.description}
-                  </p>
-                </div>
-              </li>
-            )
-          }
-          return null
-        })}
-      </ul>
+                  <div className="technology-item-bottom">
+                    <h3 className="technology-header">
+                      {technology.node.title}
+                    </h3>
+                    <p className="technology-description">
+                      {technology.node.description.description}
+                    </p>
+                  </div>
+                </li>
+              )
+            }
+            return null
+          })}
+        </ul>
+      </div>
     </Layout>
   )
 }
