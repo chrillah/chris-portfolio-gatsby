f2242ed2cc83e2297cb77841f1aa8913b90b1ade
chrillah <ch.ronnberg@gmail.com>
2024-04-25 12:19:14 +0200

    update

--- src/components/buttons/AppButton.jsx
+++ src/components/buttons/AppButton.jsx
@@ -42,6 +42,9 @@ export const LinkButton_1 = styled(Link)`
   &:hover {
     transform: scaleY(1.05);
     background: rgba(255, 255, 255, 0.3);
+    box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
+      rgba(0, 0, 0, 0.3) 0px 30px 60px -30px,
+      rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset;
   }
 
   &:active {
@@ -71,12 +74,18 @@ export const LinkButton_2 = styled(Link)`
   &:hover {
     transform: scaleY(1.05);
     background: rgba(255, 255, 255, 0.3);
+    box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
+      rgba(0, 0, 0, 0.3) 0px 30px 60px -30px,
+      rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset;
   }
 
   &:active {
     transform: scaleY(0.95);
     background: rgba(255, 255, 255, 0.3);
   }
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
+    rgba(0, 0, 0, 0.3) 0px 30px 60px -30px,
+    rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset;
 `
 
 export const BackButton = styled.button`
