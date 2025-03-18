c9d5e466d370706059860949b67b2ddb76c3c6ab
chrillah <ch.ronnberg@gmail.com>
2025-03-18 19:34:14 +0100

    chore: colors updated

--- src/themes/theme.js
+++ src/themes/theme.js
@@ -2,6 +2,7 @@ export const themeColors = {
   black: "#111111",
   peach: "#ff906f",
   lilac: "#B7AEF1",
+  pink: "#fcb8ad",
 }
 
 export const themeFonts = {
@@ -14,7 +15,7 @@ export const theme = {
   appColor: themeColors.black,
   appFont: themeFonts.poppins,
   appDisplayFont: themeFonts.delaGothic,
-  appBg: themeColors.lilac,
+  appBg: themeColors.pink,
   appGap: "0.5rem",
   appPad: "0.2rem",
 }
